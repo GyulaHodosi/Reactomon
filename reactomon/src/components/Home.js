84d7f593c04358484553db290dc69a549e660919
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-02 19:58:47 +0200

    Create Home component and style for it's background

--- reactomon/src/components/Home.js
+++ reactomon/src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Background from "../styled_components/Background";
 
 function Home() {
-  return <h3>Home Page</h3>;
+  return (
+    <div>
+      <Background>
+        <div className="message-container">
+          <h3>Welcome to Reactomon!</h3>
+          <h4>
+            On this page you can find all the pokemons and all the information
+            you need about them!
+          </h4>
+          <h3>Have fun!</h3>
+        </div>
+      </Background>
+    </div>
+  );
 }
 
 export default Home;
---
+++ reactomon/src/styled_components/Background.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Background = styled.div`
+  background-image: url(https://images7.alphacoders.com/592/thumb-1920-592678.jpg);
+  widht: 100%;
+  height: 1128px;
+  padding-top: 50px;  
+
+  .message-container {
+    z-index: 0;
+    position: relative;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 20px;
+    margin-left: 25%;
+    margin-top: 15%;
+    width: 50%;
+    height: 30%;
+    text-align: center;
+    font-family: "Lucida Console", Charcoal, sans-serif;
+    -webkit-text-stroke: 2px;
+    font-size: 30px;
+    position: absolute;
+    vertical-align: middle; 
+`;
+
+export default Background;
