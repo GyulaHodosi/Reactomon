dadaa024dcbdbd5d9db3f3ee9ea5abfa21d67b65
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-02 19:59:07 +0200

    Create styled-components for card and grid

---
+++ reactomon/src/styled_components/Card.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+//background: #ff6961;
+
+const Card = styled.div`
+  background-image: url("https://i.pinimg.com/originals/95/fc/30/95fc304b40461a9922bd1d3aff885496.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 300px 300px;
+  border-radius: 5px;
+  text-align: center;
+  height: 300px;
+  width: 300px;
+
+  .poke-name {
+    font-family: "Lucida Console", Charcoal, sans-serif;
+    padding-top: 0%;
+    color: #ac7339;
+    -webkit-text-stroke: 2px;
+    text-decoration: none;
+  }
+
+  .poke-img {
+    height: 50%;
+    width: 50%;
+    padding-top: 10%;
+  }
+`;
+
+export default Card;
---
+++ reactomon/src/styled_components/Grid.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  margin: 0px;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
+  padding-top: 70px;
+`;
+
+/*const Row = styled.div`
+  display: flex;
+`;
+
+const Col = styled.div`
+  flex: ${(props) => props.size};
+`;*/
+
+export default Grid;
