5d84e3a8c2ec964f49a30130d4847f5c48d86c75
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-02 19:59:36 +0200

    Add logo and animation to navbar

--- reactomon/src/components/Navbar.js
+++ reactomon/src/components/Navbar.js
@@ -5,14 +5,23 @@ function Navbar() {
   return (
     <nav>
       <NavLink className="nav-link" to="/">
+        <img
+          className="nav-logo"
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/4/4c/Pokeball.png"
+          }
+          alt={"logo"}
+        />
+      </NavLink>
+      <NavLink className="nav-link" to="/">
         <h1>Reactomon</h1>
       </NavLink>
       <ul className="nav-links">
         <NavLink className="nav-link" to="/pokemons">
-          <li>Pokemons</li>
+          <h2>Pokemons</h2>
         </NavLink>
         <NavLink className="nav-link" to="/types">
-          <li>Types</li>
+          <h2>Types</h2>
         </NavLink>
       </ul>
     </nav>
