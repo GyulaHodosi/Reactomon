45c0967de8b34fe85a39e0fd45e1fffe104ef751
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-03 19:59:29 +0200

    Implement Button and TypeCard styled components

---
+++ reactomon/src/styled_components/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.div`
+  background-color: #fccf00;
+  text-align: center;
+  display: inline-block;
+  margin: 10px;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  color: #2468b1;
+  padding: 10px;
+  &:hover {
+    background-color: #ff1a1a;
+    cursor: pointer;
+  }
+  font-family: "Lucida Console", Charcoal, sans-serif;
+  font-size: 30px;
+  -webkit-text-stroke: 2px;
+`;
+
+export default Button;
---
+++ reactomon/src/styled_components/TypeCard.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Type = styled.div`
+  color: #ac7339;
+  text-align: center;
+  font-family: "Lucida Console", Charcoal, sans-serif;
+  -webkit-text-stroke: 2px;
+  padding: 25px;
+  &:hover {
+    color: #ff1a1a;
+  }
+`;
+
+export default Type;
